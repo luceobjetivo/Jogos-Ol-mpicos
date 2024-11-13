d08010e7ea8b45ca77a91d7b210c612e9dbda337
luceobjetivo <luceobjetivo@gmail.com>
2024-11-13 10:34:13 -0300

    Add files via upload

---
+++ JS/script (copy).js
@@ -0,0 +1,13 @@
+.Controles {
+margin: 20px 60px 30px 100px;
+padding: 3px;
+display:flex;
+font-size: 1.5rem;
+width: 60vw;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+border-radius: 2%;
+border-color: rgb(30, 30, 30);
+border-width: 5px;
+  // Rolar a tela para baixo window.scrollBy(0, 100); // Rola 100 pixels para baixo // Rolar a tela para cima window.scrollBy(0, -100); // Rola 100 pixels para cima 
